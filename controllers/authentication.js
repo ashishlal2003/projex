06b9310a514fee085b1acee32d42be8c99405280
Ashish Lal <ashishlal2003@gmail.com>
2023-04-07 23:22:39 +0530

    changes2

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,3 +1,6 @@
-module.exports.getAuth = (req,res,next) =>{
+const cookies = require('../util/cookie');
+const User = require('../models/user');
+
+module.exports.getAuth = (req,res,next)=>{
     res.render('authentication');
-}
+};
---
+++ models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+
+    projects:[{
+        productId: {
+            type: Schema.Types.ObjectId,
+            ref: 'Product',
+            required: true
+        }
+    }]
+});
+
+//Project addition methods pending
+
+module.exports = mongoose.model('User', userSchema);
